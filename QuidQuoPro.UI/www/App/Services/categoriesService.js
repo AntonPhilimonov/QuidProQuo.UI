f546cc6db4633f98084c072ae1353bc0f5ec8b5a
Nat.Zybova <Nat.Zybova@yandex.ru>
2017-08-26 18:40:01 +0400

    оживление доски объявлений

--- QuidQuoPro.UI/www/App/Services/categoriesService.js
+++ QuidQuoPro.UI/www/App/Services/categoriesService.js
@@ -2,11 +2,11 @@
     .service('CategoriesService', function () {
         this.getCategories = function (categoryType) {
             var items = [];
-            $.get('http://localhost/api/categories/', function (data) {
+            $.get('http://localhost/api/category/', function (data) {
                 for (var i = 0; i < data.length; i++) {
                     if (data[i].IsCategory === true && data[i].CategoryType === categoryType) {
                         items.push(data[i]);
-                    }
+                     }
                 }
             },
            'json');
--- QuidQuoPro.UI/www/App/Views/ads/adsController.js
+++ QuidQuoPro.UI/www/App/Views/ads/adsController.js
@@ -1,7 +1,15 @@
 ﻿angular.module('app.controllers')
-    .controller('AdsController', function ($scope) {
-        function onCreate(parameters) {
-            $scope.title = i18n.t('tabs.tab-ads-title');
+    .controller('AdsController', function ($scope, AdsService) {
+    function onCreate(parameters) {
+
+        $scope.title = i18n.t('tabs.tab-ads-title');
+
+        var promiseObj = AdsService.getOrders();
+        promiseObj.then(function (value) {
+            $scope.orders = value;
+        });
+
         }
         onCreate();
     });
+
